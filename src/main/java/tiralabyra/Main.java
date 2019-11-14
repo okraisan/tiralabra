2e80e7bbe30dfadc16e474ad59a79ccf661795ef
Oona Räisänen <okraisan@protonmail.com>
2019-11-14 23:16:58 +0200

    checkstyle fixes

--- src/main/java/tiralabyra/Main.java
+++ src/main/java/tiralabyra/Main.java
@@ -6,23 +6,23 @@ import java.util.Queue;
 public class Main {
 
   public static void main(String[] args) {
-    
+
     final int backtrackColor = 0xFFFF0000;
     final String inFileName  = "C:\\Users\\windy\\Pictures\\labyrintti.png";
     final String outFileName = "C:\\Users\\windy\\Pictures\\labyra_out.png";
 
     Image image = new tiralabyra.Image(inFileName);
     ConnectedGraph graph = new tiralabyra.ConnectedGraph(image.getNumberOfPixels());
-    
+
     int entryNodeIndex = -1;
     int exitNodeIndex  = -1;
-    
+
     Point  E = new Point(1,  0);
     Point SE = new Point(1,  1);
     Point S  = new Point(0,  1);
     Point SW = new Point(-1, 1);
     Point  W = new Point(-1, 0);
-    
+
     // Assign a neighborhood to all non-wall pixels.
     for (Point point : image.getPixelPositions()) {
       if (image.isTraversableAt(point)) {
@@ -31,20 +31,20 @@ public class Main {
           graph.addNeighborhood(image.getIndexForPixel(point),
                                 image.getIndexForPixel(Point.add(point, E)));
         }
-        
+
         // Non-wall pixel to the South is always traversable.
         if (image.isTraversableAt(Point.add(point, S))) {
           graph.addNeighborhood(image.getIndexForPixel(point),
                                 image.getIndexForPixel(Point.add(point, S)));
         }
-        
+
         // South-west pixel is traversable unless there's a corner in between.
         if (image.isTraversableAt(Point.add(point, SW))
             && image.isTraversableAt(Point.add(point,  W)) && image.isTraversableAt(Point.add(point, E))) {
           graph.addNeighborhood(image.getIndexForPixel(point),
                                 image.getIndexForPixel(Point.add(point, SW)));
         }
-        
+
         // South-east pixel is traversable unless there's a corner in between.
         if (image.isTraversableAt(Point.add(point, SE))
             && image.isTraversableAt(Point.add(point,  S)) && image.isTraversableAt(Point.add(point, E))) {
@@ -52,7 +52,7 @@ public class Main {
                                 image.getIndexForPixel(Point.add(point, SE)));
         }
       }
-      
+
       if (image.hasEntryPointAt(point)) {
         entryNodeIndex = image.getIndexForPixel(point);
       }
@@ -60,7 +60,7 @@ public class Main {
         exitNodeIndex = image.getIndexForPixel(point);
       }
     }
-    
+
     if (entryNodeIndex == -1) {
       System.out.println("Labyrinth has no entry point. Please color one pixel as #FF0000 (" + 0xff000000 + ")");
       return;
@@ -69,11 +69,11 @@ public class Main {
       System.out.println("Labyrinth has no exit point. Please color one pixel as #00FF00.");
       return;
     }
-    
+
     // Breadth-first search.
     boolean[] isVisited = new boolean[image.getNumberOfPixels()];
     int[]     parent    = new int[image.getNumberOfPixels()];
-    
+
     Queue<Integer> fillQueue = new LinkedList<Integer>();
     fillQueue.add(new Integer(entryNodeIndex));
 
@@ -91,14 +91,14 @@ public class Main {
         }
       }
     }
-    
+
     // Backtrack.
     int backtrackIndex = exitNodeIndex;
     while (backtrackIndex != entryNodeIndex) {
       image.plotPathAroundIndex(backtrackIndex, backtrackColor);
       backtrackIndex = parent[backtrackIndex];
     }
-    
+
     image.save(outFileName);
   }
 }
--- src/main/java/tiralabyra/Node.java
+++ src/main/java/tiralabyra/Node.java
@@ -1,29 +1,38 @@
 package tiralabyra;
 
-import java.util.*;
+import java.util.Set;
+import java.util.HashSet;
 
 /**
  * Represents a traversable (non-wall) pixel in the input image.
  * @author okraisan
  */
 public class Node {
+  /**
+   * Set of neighbors to this node, as integer indices.
+   */
   private Set<Integer> neighbors;
-  
+
   /**
    * Initializes the Node to have no neighbors.
    */
   public Node() {
     neighbors = new HashSet<Integer>();
   }
-  
+
   /**
    * Sets one of this node's neighbors to point to an index in the graph.
    * @param index         Index of the neighbor node in the graph.
    */
-  public void addNeighbor(int index) {
+  public void addNeighbor(final int index) {
     neighbors.add(index);
   }
-  
+
+  /**
+   * Return the set of neighbors to this node.
+   *
+   * @return Set of indices to neighbor nodes.
+   */
   public Set<Integer> getNeighbors() {
     return neighbors;
   }
