bd062675377f45e40f64575e39425e128a881556
Oona Räisänen <okraisan@protonmail.com>
2019-11-22 16:52:44 +0200

    JavaDoc comments

--- src/main/java/tiralabyra/SolvedResult.java
+++ src/main/java/tiralabyra/SolvedResult.java
@@ -6,6 +6,10 @@ public class SolvedResult {
    * Was the labyrinth successfully solved?
    */
   private boolean solved = false;
+
+  /**
+   * Backtrack path containing the parent index for each index.
+   */
   private int[] parents;
 
   /**
@@ -23,19 +27,31 @@ public class SolvedResult {
     solved = sol;
     parents = par;
   }
-  
-  public void setSolved(boolean sol) {
+
+  /**
+   * @param sol Was the labyrinth successfully solved?
+   */
+  public void setSolved(final boolean sol) {
     solved = sol;
   }
-  
-  public void setParents(int[] par) {
+
+  /**
+   * @param par Backtrack path containing the parent index for each index.
+   */
+  public void setParents(final int[] par) {
     parents = par;
   }
-  
+
+  /**
+   * @return Was the labyrinth successfully solved?
+   */
   public boolean wasSolved() {
     return solved;
   }
-  
+
+  /**
+   * @return Backtrack path containing the parent index for each index.
+   */
   public int[] getParents() {
     return parents;
   }
