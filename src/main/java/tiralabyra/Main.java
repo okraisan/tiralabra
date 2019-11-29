f89296aea573b79be5b71249c6f465e6a9ba9c4c
Oona Räisänen <okraisan@protonmail.com>
2019-11-29 14:38:36 +0200

    print stats of main execution

--- src/main/java/tiralabyra/Main.java
+++ src/main/java/tiralabyra/Main.java
@@ -15,21 +15,32 @@ public final class Main {
 
     ConnectedGraph graph = new tiralabyra.ConnectedGraph(image);
     AStar astar = new AStar();
+
+    long startTime = System.nanoTime();
     SolvedResult result = astar.solve(graph);
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime);
 
     if (result.wasSolved()) {
+      System.out.println("Solved!");
+      System.out.println();
+      System.out.println("RESULTS\n=======");
+      System.out.println(String.format("Input size: %.1f Mpx", image.getNumberOfPixels() / 1000000.0));
+      System.out.println(String.format("Elapsed:    %.0f ms", duration / 1000000.0));
+
       // Backtrack.
 
       int[] parent = result.getParents();
 
       int backtrackIndex = graph.getExitNodeIndex();
       while (backtrackIndex != graph.getEntryNodeIndex()) {
-        image.plotPathAroundIndex(backtrackIndex, 0, backtrackColor);
+        image.plotPathAroundIndex(backtrackIndex, 1, backtrackColor);
         backtrackIndex = parent[backtrackIndex];
       }
 
-      System.out.println("Solved; path length " + result.getLength() + " px");
+      System.out.println(String.format("Length:     %.1f px", result.getLength()));
       image.save(outFileName);
+      System.out.println("Output:     " + outFileName);
     } else {
       System.out.println("This labyrinth can't be solved.");
     }
