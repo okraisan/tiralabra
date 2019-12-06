3dc5938b0671a1b9655c18003f237fb823193d02
Oona Räisänen <okraisan@protonmail.com>
2019-12-06 23:38:18 +0200

    checkstyle fixes

--- src/test/java/tiralabyra/PrioNodeTest.java
+++ src/test/java/tiralabyra/PrioNodeTest.java
@@ -1,47 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tiralabyra;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
- *
- * @author windy
+ * Tests related to nodes in the priority queue (minimum heap).
  */
 public class PrioNodeTest {
-    
-    PrioNode firstNode;
-    PrioNode secondNode;
-    PrioNode anotherSecondNode;
-    PrioNode thirdNode;
-    
-    public PrioNodeTest() {
-    }
-    
-    @Before
-    public void setUp() {
-      firstNode = new PrioNode(0, 0.0);
-      secondNode = new PrioNode(1, 1.0);
-      anotherSecondNode = new PrioNode(0, 1.0);
-      thirdNode = new PrioNode(1, 2.0);
-    }
+  PrioNode firstNode;
+  PrioNode secondNode;
+  PrioNode anotherSecondNode;
+  PrioNode thirdNode;
 
-    /**
-     * Test that ordering is compared correctly.
-     */
-    @Test
-    public void testCompareTo() {
-      assertEquals(-1, firstNode.compareTo(secondNode));
-      assertEquals(0, secondNode.compareTo(anotherSecondNode));
-      assertEquals(1, thirdNode.compareTo(secondNode));
-    }
+  public PrioNodeTest() {
+  }
+
+  /**
+    * Set up some test nodes with a known order but differing contents.
+   */
+  @Before
+  public void setUp() {
+    firstNode = new PrioNode(0, 0.0);
+    secondNode = new PrioNode(1, 1.0);
+    anotherSecondNode = new PrioNode(0, 1.0);
+    thirdNode = new PrioNode(1, 2.0);
+  }
+
+  /**
+   * Test that ordering is compared correctly.
+   */
+  @Test
+  public void testCompareTo() {
+    assertEquals(-1, firstNode.compareTo(secondNode));
+    assertEquals(0, secondNode.compareTo(anotherSecondNode));
+    assertEquals(1, thirdNode.compareTo(secondNode));
+  }
     
 }
--- src/test/java/tiralabyra/QueueTest.java
+++ src/test/java/tiralabyra/QueueTest.java
@@ -1,60 +1,64 @@
 package tiralabyra;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class QueueTest {
-    Queue queue;
-    Queue emptyQueue;
-    
-    public QueueTest() {
-    }
-    
-    @Before
-    public void setUp() {
-      queue = new Queue();
-      emptyQueue = new Queue();
-      queue.insert(1);
-      queue.insert(2);
-      queue.insert(3);
-      queue.insert(4);
-      queue.insert(5);
-    }
+  Queue queue;
+  Queue emptyQueue;
 
-    /**
-     * Test of size method, of class Queue.
-     */
-    @Test
-    public void testSize() {
-      assertEquals(0, emptyQueue.size());
-      assertEquals(5, queue.size());
-    }
+  public QueueTest() {
+  }
 
-    /**
-     * Test of insert method, of class Queue.
-     */
-    @Test
-    public void testInsert() {
-      emptyQueue.insert(1);
-      int fromQueue = emptyQueue.pop();
-      assertEquals(1, fromQueue);
-      assertEquals(0, emptyQueue.size());
-    }
+  /**
+   * Set up queues with known contents, size, and ordering.
+   */
+  @Before
+  public void setUp() {
+    queue = new Queue();
+    emptyQueue = new Queue();
+    queue.insert(1);
+    queue.insert(2);
+    queue.insert(3);
+    queue.insert(4);
+    queue.insert(5);
+  }
 
-    /**
-     * Test of pop method, of class Queue.
-     */
-    @Test
-    public void testPopOrder() {
-      assertEquals(1, queue.pop());
-      assertEquals(2, queue.pop());
-      assertEquals(3, queue.pop());
-      assertEquals(4, queue.pop());
-      assertEquals(5, queue.pop());
-    }
-    
+  /**
+   * Test of size method, of class Queue.
+   */
+  @Test
+  public void testSize() {
+    assertEquals(0, emptyQueue.size());
+    assertEquals(5, queue.size());
+  }
+
+  /**
+   * Test of insert method, of class Queue.
+   */
+  @Test
+  public void testInsert() {
+    emptyQueue.insert(1);
+    int fromQueue = emptyQueue.pop();
+    assertEquals(1, fromQueue);
+    assertEquals(0, emptyQueue.size());
+  }
+
+  /**
+   * Test of pop method, of class Queue.
+   */
+  @Test
+  public void testPopOrder() {
+    assertEquals(1, queue.pop());
+    assertEquals(2, queue.pop());
+    assertEquals(3, queue.pop());
+    assertEquals(4, queue.pop());
+    assertEquals(5, queue.pop());
+  }
+
 }
